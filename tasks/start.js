0f2150099dd5979df12d275af556761681e492de
Kevin Kim <ykk3416@gmail.com>
2021-02-21 19:01:45 -0500

    Remove deploy from start

--- tasks/start.js
+++ tasks/start.js
@@ -1,13 +1,11 @@
 const { sh } = require('tasksfile');
-const prisma = require('./prisma');
 
 function dev() {
   return sh('nodemon -r dotenv/config -r esm src/index.js', { nopipe: true });
 }
 
-async function start() {
-  await prisma.deploy();
-  await sh('node -r esm src/index.js', { nopipe: true });
+function start() {
+  return sh('node -r esm src/index.js', { nopipe: true });
 }
 
 module.exports = {
