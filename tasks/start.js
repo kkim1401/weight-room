4ec6863292ca5a43f21bbab40def11fdc6c402ae
Kevin Kim <ykk3416@gmail.com>
2021-02-21 19:04:01 -0500

    Add generate to start

--- tasks/start.js
+++ tasks/start.js
@@ -1,11 +1,13 @@
 const { sh } = require('tasksfile');
+const { generate } = require('./prisma');
 
 function dev() {
   return sh('nodemon -r dotenv/config -r esm src/index.js', { nopipe: true });
 }
 
-function start() {
-  return sh('node -r esm src/index.js', { nopipe: true });
+async function start() {
+  await generate();
+  await sh('node -r esm src/index.js', { nopipe: true });
 }
 
 module.exports = {
