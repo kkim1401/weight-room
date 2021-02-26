11f9157200514ab09f37275e6233384cfd5c7117
Kevin Kim <ykk3416@gmail.com>
2021-02-25 20:16:34 -0500

    Replace push with dev

--- tasks/start.js
+++ tasks/start.js
@@ -1,12 +1,12 @@
 const { sh } = require('tasksfile');
-const { push } = require('./prisma');
+const { dev } = require('./prisma');
 
 function dev() {
   return sh('nodemon -r dotenv/config -r esm src/index.js', { nopipe: true });
 }
 
 async function start() {
-  await push();
+  await dev();
   await sh('node -r esm src/index.js', { nopipe: true });
 }
 
