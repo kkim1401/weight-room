9587fee60e099eb9db3e55d717ee1067aaf5fac9
Kevin Kim <ykk3416@gmail.com>
2021-02-25 20:11:51 -0500

    Add push to start

--- tasks/start.js
+++ tasks/start.js
@@ -1,11 +1,13 @@
 const { sh } = require('tasksfile');
+const { push } = require('./prisma');
 
 function dev() {
   return sh('nodemon -r dotenv/config -r esm src/index.js', { nopipe: true });
 }
 
-function start() {
-  return sh('node -r esm src/index.js', { nopipe: true });
+async function start() {
+  await push();
+  await sh('node -r esm src/index.js', { nopipe: true });
 }
 
 module.exports = {
